723542a92f930c85a7b87115cf1be12b21726246
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-06 22:07:02 -0500

    corrijo error print

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main //Forma de crear modulos en go
 
 import "fmt"
 
-func main()  {
-  var name string
-  fmt.Println("Hola Mundo")
-  fmt.Print("Ingresa tu nombre: ")
-  fmt.Scanf("%s", &name)
-  fmt.Printf("Has ingresado como: %s --- Bienvenido\n",name)d
+func main() {
+	var name string
+	fmt.Println("Hola Mundo")
+	fmt.Print("Ingresa tu nombre: ")
+	fmt.Scanf("%s", &name)
+	fmt.Printf("Has ingresado como: %s --- Bienvenido\n", name)
 }
