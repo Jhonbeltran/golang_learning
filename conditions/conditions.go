ac1f7c222110210df519640f126ce2ab1ed6d0bd
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-11 13:23:46 -0500

    For

--- conditions/conditions.go
+++ conditions/conditions.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	iffunc()
+	forfunc()
+}
+
+func iffunc() {
 	var number int
+	fmt.Println("********** IF **********")
 	fmt.Print("Ingresa un número: ")
 	fmt.Scanf("%d", &number)
 
@@ -19,5 +25,34 @@ func main() {
 		fmt.Println("Diferentes")
 	} else {
 		fmt.Println("Iguales")
+	}
+}
+
+func forfunc() {
+	fmt.Println("********** FOR **********")
+	for i := 0; i < 20; i++ {
+		fmt.Printf("%d, ", i)
+	}
+	fmt.Print("\n")
+
+	fmt.Println("*** For Similar While ***")
+	fmt.Println("condición definida c > 0")
+	c := 100
+	for c > 0 {
+		c -= 10
+		fmt.Println("Valor de c = ", c)
+	}
+
+	fmt.Println("*** For Similar While ***")
+	fmt.Println("Condición definida internamente")
+	s := 1000
+	for {
+		s -= 10
+		fmt.Printf("%d, ", s)
+		//Condición para terminar
+		if s == 0 {
+			fmt.Println("\nTermina el for 'infinito' break")
+			break
+		}
 	}
 }
