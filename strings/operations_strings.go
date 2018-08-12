d826191d57a07ccc888b5d6d07d4d206193f4075
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-11 22:25:22 -0500

    Agrego tratamiento de Strings

---
+++ strings/operations_strings.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var text = "hola mundo, hola platzi, Hola Go"
+	fmt.Println(text)
+	fmt.Println(strings.ToUpper(text))
+	fmt.Println(strings.ToLower(text))
+	fmt.Println(strings.ToTitle(text))
+	//-1 para que cambien todas las ocurrencias
+	fmt.Println(strings.Replace(text, "hola", "Hi", -1))
+
+	text_array := strings.Split(text, " ")
+	fmt.Println(text_array[0])
+	fmt.Println(text_array, len(text_array))
+
+}
