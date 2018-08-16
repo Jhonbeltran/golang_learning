3470c9f25b38fb2c12d79f06d9f9e18ae1422b17
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-16 12:20:34 -0500

    Cambio en punteros

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -6,6 +6,12 @@ func PrintPointers() {
 	variable := 100
 	var puntero *int
 	puntero = &variable
+	fmt.Println("Sin Modificar")
+	fmt.Println(variable, puntero)
+	fmt.Println(variable, *puntero)
+	fmt.Println(&variable, puntero)
+	*puntero = 300
+	fmt.Println("Modificado")
 	fmt.Println(variable, puntero)
 	fmt.Println(variable, *puntero)
 	fmt.Println(&variable, puntero)
